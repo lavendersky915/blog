0179191577dd711f9f2253b4f2d434bd8c5c825c
lavender <lavendersky915@gmail.com>
2013-01-07 01:57:39 +0800

    ss
    ss

--- blog.py
+++ blog.py
@@ -29,12 +29,11 @@ import json
 
 from tornado.options import define, options
 
-# define("port", default=8887, help="run on the given port", type=int)
+define("port", default=8888, help="run on the given port", type=int)
 define("mysql_host", default="127.0.0.1:3306", help="blog database host")
 define("mysql_database", default="blog", help="blog database name")
 define("mysql_user", default="root", help="blog database user")
 define("mysql_password", default="19052260", help="blog database password")
-
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -266,7 +265,8 @@ class EntryModule(tornado.web.UIModule):
 def main():
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(Application())
-    http_server.listen(int(sys.argv[1]))
+    # http_server.listen(int(sys.argv[1]))
+    http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
 
 
