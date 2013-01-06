c395ef1f72cb06dc82297e2583c95fc8deadccac
lavender <lavendersky915@gmail.com>
2013-01-07 02:02:53 +0800

    dsf
    sdf

--- blog.py
+++ blog.py
@@ -31,7 +31,7 @@ from tornado.options import define, options
 
 define("port", default=8888, help="run on the given port", type=int)
 define("mysql_host", default="127.0.0.1:3306", help="blog database host")
-define("mysql_database", default="blog", help="blog database name")
+define("mysql_database", default="blog2", help="blog database name")
 define("mysql_user", default="root", help="blog database user")
 define("mysql_password", default="19052260", help="blog database password")
 
