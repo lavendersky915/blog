6cbbecdcb17a5dd16246807e11e733cfe951b2f1
lavender <lavendersky915@gmail.com>
2013-02-24 22:03:24 +0800

    dasd
    asdas

--- blog.py
+++ blog.py
@@ -191,7 +191,7 @@ class GoogleHandler(BaseHandler):
         count = result.count('items')
         obj_result = tornado.escape.json_decode(result)
         content = ""
-        for x in xrange(0,1):
+        for x in xrange(0,0):
             html = obj_result['items'][x]['link']
             link =str(html) 
             crl = pycurl.Curl()
