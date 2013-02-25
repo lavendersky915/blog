aeaae269d2754470a301c670f664c489dc502cec
lavender <lavendersky915@gmail.com>
2013-02-25 15:39:11 +0800

    back

--- blog.py
+++ blog.py
@@ -205,13 +205,23 @@ class GoogleHandler(BaseHandler):
             crl.perform()               
         
             
+            soup = BeautifulSoup(crl.fp.getvalue())
+            for each in soup:
 
+
+                 ans = soup.find("div", { "class" : "patent_bibdata" })
+                
+
+                content = soup.prettify()
+
+            
+            pass
             
         pass
 
-         data = tornado.escape.json_encode(crl.fp.getvalue())
+         data = tornado.escape.json_encode(content)
         #self.render("google.html", entries="test")
-        self.write(data)
+        self.write(content)
 
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -229,19 +239,19 @@ def strip_tags(html):
 
 class TestHandler(BaseHandler):
     def get(self):
-	url = "http://www.google.com/"
+    url = "http://www.google.com/"
 
 #buf = cStringIO.StringIO()
-	crl = pycurl.Curl()
-	crl.setopt(pycurl.VERBOSE,1)
-	crl.setopt(pycurl.FOLLOWLOCATION, 1)
-	crl.setopt(pycurl.MAXREDIRS, 5)
-	crl.fp = StringIO.StringIO()
-	crl.setopt(pycurl.URL, url)
-	crl.setopt(crl.WRITEFUNCTION, crl.fp.write)
-	crl.perform()
-	#print crl.fp.getvalue()
- 	self.write(crl.fp.getvalue())
+    crl = pycurl.Curl()
+    crl.setopt(pycurl.VERBOSE,1)
+    crl.setopt(pycurl.FOLLOWLOCATION, 1)
+    crl.setopt(pycurl.MAXREDIRS, 5)
+    crl.fp = StringIO.StringIO()
+    crl.setopt(pycurl.URL, url)
+    crl.setopt(crl.WRITEFUNCTION, crl.fp.write)
+    crl.perform()
+    #print crl.fp.getvalue()
+    self.write(crl.fp.getvalue())
         #req = urllib2.Request('http://www.citytalk.tw/cata/')
      #   response = urllib2.urlopen('http://www.google.com/patents/US6658577')
       #  the_page = response.read()
