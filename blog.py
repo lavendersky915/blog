aceff59b98d6dece9bfdc8a8ac27054f51b9b237
lavender <lavendersky915@gmail.com>
2013-03-06 13:09:51 +0800

    5
    5

--- blog.py
+++ blog.py
@@ -210,7 +210,7 @@ class GoogleHandler(BaseHandler):
 
         data = tornado.escape.json_encode(crl.fp.getvalue())
         #self.render("google.html", entries="test")
-        self.write(crl.fp.getvalue())
+        self.write(data)
 class MLStripper(HTMLParser):
     def __init__(self):
         self.reset()
