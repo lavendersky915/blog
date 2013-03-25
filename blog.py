1489f02ea71effb5492aac6d9fe461e6efe6528b
lavender <lavendersky915@gmail.com>
2013-03-25 13:31:44 +0800

    ss

--- blog.py
+++ blog.py
@@ -76,106 +76,8 @@ class Application(tornado.web.Application):
         self.db = tornado.database.Connection(
             host=options.mysql_host, database=options.mysql_database,
             user=options.mysql_user, password=options.mysql_password)
-class BaseHandler(tornado.web.RequestHandler):
-    @property
-    def db(self):
-        return self.application.db
-
-    def get_current_user(self):
-        user_id = self.get_secure_cookie("user")
-
-        print "====== the user id is .... "
-        print user_id
-        if not user_id:
-            return None
-
-        return self.db.get("SELECT * FROM authors WHERE id = %s", int(user_id))
-
-class SampleApp(BaseHandler):
-    def get(self):
-        self.render('blog_sample.html')
-class ApiBlogPosts(BaseHandler):
-    def get(self):
-        _entries = self.db.query("SELECT * FROM entries ORDER BY published "
-                                "DESC LIMIT 10")
-        #entries = json.dumps(entries)
-        entries_api = []
-        entries = {}
-        for e in _entries:
-            print "====="
-            _e = e
-            _e['updated'] = str(_e['updated'])
-            _e['published'] = str(_e['published'])
-            entries_api.append(_e)
-            print "====="
-        print entries_api
-        self.set_header("Content-Type", "application/json")
-        self.write({'data':entries_api})
-    def post(self):
-        # allows third part apps to create blog posts....
-        self.set_header("Content-Type", "application/json")
-        '''
-            but for simplicity sake, when testing out, will need to login to google account first.....
-        '''
-        user = 1
-        title = self.get_argument("title")
-        text = self.get_argument("markdown")
-        html = markdown.markdown(text)
-
-        print "================================================================"
-        print user
-        print title
-        print text
-        print "================================================================"
-        response = {'success':True}
-        try:
-            slug = unicodedata.normalize("NFKD", title).encode("ascii", "ignore")
-            slug = re.sub(r"[^\w]+", " ", slug)
-            slug = "-".join(slug.lower().strip().split())
-            if not slug: slug = "entry"
-            while True:
-                e = self.db.get("SELECT * FROM entries WHERE slug = %s", slug)
-                if not e: break
-                slug += "-2"
-            self.db.execute(
-                "INSERT INTO entries (author_id,title,slug,markdown,html,"
-                "published) VALUES (%s,%s,%s,%s,%s,UTC_TIMESTAMP())",
-                user, title, slug, text, html)
-            self.write({'data':response})
-        except:
-            response['success'] = False
-            response['error_message'] = "You screwed up!"
-            self.write({'data':response})
-class HomeHandler(BaseHandler):
-    def get(self):
-        entries = self.db.query("SELECT * FROM entries ORDER BY published "
-                                "DESC LIMIT 5")
-        if not entries:
-            self.redirect("/compose")
-            return
-        self.render("home.html", entries=entries)
 
 
-class EntryHandler(BaseHandler):
-    def get(self, slug):
-        entry = self.db.get("SELECT * FROM entries WHERE slug = %s", slug)
-        if not entry: raise tornado.web.HTTPError(404)
-        self.render("entry.html", entry=entry)
-
-
-class ArchiveHandler(BaseHandler):
-    def get(self):
-        entries = self.db.query("SELECT * FROM entries ORDER BY published "
-                                "DESC")
-        self.render("archive.html", entries=entries)
-
-
-class FeedHandler(BaseHandler):
-    def get(self):
-        entries = self.db.query("SELECT * FROM entries ORDER BY published "
-                                "DESC LIMIT 10")
-        self.set_header("Content-Type", "application/atom+xml")
-        self.render("feed.xml", entries=entries)
 class GoogleHandler(BaseHandler):
     def get(self):
         keyword = self.get_argument("keyword", default=None, strip=False)
@@ -247,7 +149,7 @@ class Lavender_STPI(BaseHandler):
                 liticom = litiname[1].split('提告日期')
                 twocom = liticom[0].split('v.')
                 p.append(twocom[0])
-                
+
             pass
             pass
         
@@ -302,92 +204,6 @@ class TestHandler(BaseHandler):
 
         self.write(crl.fp.getvalue())
        
-class ComposeHandler(BaseHandler):
-    @tornado.web.authenticated
-    def get(self):
-        id = self.get_argument("id", None)
-        entry = None
-        if id:
-            entry = self.db.get("SELECT * FROM entries WHERE id = %s", int(id))
-        self.render("compose.html", entry=entry)
-
-    @tornado.web.authenticated
-    def post(self):
-        id = self.get_argument("id", None)
-        title = self.get_argument("title")
-        text = self.get_argument("markdown")
-        html = markdown.markdown(text)
-        if id:
-            entry = self.db.get("SELECT * FROM entries WHERE id = %s", int(id))
-            if not entry: raise tornado.web.HTTPError(404)
-            slug = entry.slug
-            self.db.execute(
-                "UPDATE entries SET title = %s, markdown = %s, html = %s "
-                "WHERE id = %s", title, text, html, int(id))
-        else:
-            slug = unicodedata.normalize("NFKD", title).encode(
-                "ascii", "ignore")
-            slug = re.sub(r"[^\w]+", " ", slug)
-            slug = "-".join(slug.lower().strip().split())
-            if not slug: slug = "entry"
-            while True:
-                e = self.db.get("SELECT * FROM entries WHERE slug = %s", slug)
-                if not e: break
-                slug += "-2"
-            self.db.execute(
-                "INSERT INTO entries (author_id,title,slug,markdown,html,"
-                "published) VALUES (%s,%s,%s,%s,%s,UTC_TIMESTAMP())",
-                self.current_user.id, title, slug, text, html)
-        self.redirect("/entry/" + slug)
-class AuthLoginHandler(BaseHandler, tornado.auth.GoogleMixin):
-    @tornado.web.asynchronous
-    def get(self):
-        if self.get_argument("openid.mode", None):
-            self.get_authenticated_user(self.async_callback(self._on_auth))
-            return
-        self.authenticate_redirect()
-
-    def _on_auth(self, user):
-        print "========================="
-        print user
-        print "========================="
-        if not user:
-            raise tornado.web.HTTPError(500, "Google auth failed")
-
-
-
-        author = self.db.get("SELECT * FROM authors WHERE email = %s",user["email"])
-        print author
-        if not author:
-            # Auto-create first author
-            any_author = self.db.get("SELECT * FROM authors LIMIT 1")
-
-            print "================================ any_author"
-            print any_author
-
-            if not any_author:
-                author_id = self.db.execute(
-                    "INSERT INTO authors (email,name) VALUES (%s,%s)",
-                    user["email"], user["name"])
-            else:
-                self.redirect("/")
-                return
-        else:
-            author_id = author["id"]
-            print "============== author_id "
-            print author_id
-
-        self.set_secure_cookie("user", str(author_id))
-        self.redirect(self.get_argument("next", "/"))
-class AuthLogoutHandler(BaseHandler):
-    def get(self):
-        self.clear_cookie("user")
-        self.redirect(self.get_argument("next", "/"))
-
-
-class EntryModule(tornado.web.UIModule):
-    def render(self, entry):
-        return self.render_string("modules/entry.html", entry=entry)
 
 def main():
     tornado.options.parse_command_line()
