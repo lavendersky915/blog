7bdc8fa299aa0d6b9d392b5760574a5e2184431f
lavender <lavendersky915@gmail.com>
2013-03-25 17:00:37 +0800

    de

--- blog.py
+++ blog.py
@@ -255,7 +255,7 @@ class Lavender_STPI(BaseHandler):
         
         
         data = tornado.escape.json_encode(de)
-        self.write(data)
+        self.write(de)
     
 
 class MLStripper(HTMLParser):
