b8a7aa2daa354404dfdeb14485362cec2b3c642e
lavender <lavendersky915@gmail.com>
2013-03-25 13:32:53 +0800

    ss

--- blog.py
+++ blog.py
@@ -76,6 +76,20 @@ class Application(tornado.web.Application):
         self.db = tornado.database.Connection(
             host=options.mysql_host, database=options.mysql_database,
             user=options.mysql_user, password=options.mysql_password)
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        return self.application.db
+
+    def get_current_user(self):
+        user_id = self.get_secure_cookie("user")
+
+        print "====== the user id is .... "
+        print user_id
+        if not user_id:
+            return None
+
+        return self.db.get("SELECT * FROM authors WHERE id = %s", int(user_id))
 
 
 class GoogleHandler(BaseHandler):
