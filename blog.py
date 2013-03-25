979cb759aee0bf94f2da4f48c242d4a86cdafb7d
lavender <lavendersky915@gmail.com>
2013-03-25 12:40:44 +0800

    ss

--- blog.py
+++ blog.py
@@ -223,8 +223,7 @@ class Lavender_STPI(BaseHandler):
         obj_result = tornado.escape.json_decode(result)
         test = ""
         litigation=0
-        plaintiff = array()
-        defendant = array()
+        t = 0
         w = unicode('告', 'utf-8')
         name = unicode('訴訟名稱', 'utf-8')
         for x in xrange(0,count):
@@ -246,7 +245,7 @@ class Lavender_STPI(BaseHandler):
                 litiname = content.split('訴訟名稱')
                 liticom = litiname[1].split('提告日期')
                 twocom = liticom[0].split('v.')
-
+                
             pass
             
             pass
