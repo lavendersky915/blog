439cacdf66de98d01ed227291124fb42e314eb84
lavender <lavendersky915@gmail.com>
2013-03-25 16:41:25 +0800

    aa

--- blog.py
+++ blog.py
@@ -77,19 +77,12 @@ class Application(tornado.web.Application):
             host=options.mysql_host, database=options.mysql_database,
             user=options.mysql_user, password=options.mysql_password)
 class BaseHandler(tornado.web.RequestHandler):
-    @property
-    def db(self):
-        return self.application.db
+    
+    a = "123"
 
-    def get_current_user(self):
-        user_id = self.get_secure_cookie("user")
-
-        print "====== the user id is .... "
-        print user_id
-        if not user_id:
-            return None
-
-        return self.db.get("SELECT * FROM authors WHERE id = %s", int(user_id))
+class HomeHandler(BaseHandler):
+    def get(self):
+        self.render("home.html")
 
 class SampleApp(BaseHandler):
     def get(self):
@@ -146,14 +139,8 @@ class ApiBlogPosts(BaseHandler):
             response['success'] = False
             response['error_message'] = "You screwed up!"
             self.write({'data':response})
-class HomeHandler(BaseHandler):
-    def get(self):
-        entries = self.db.query("SELECT * FROM entries ORDER BY published "
-                                "DESC LIMIT 5")
-        if not entries:
-            self.redirect("/compose")
-            return
-        self.render("home.html", entries=entries)
+
+
 
 
 class EntryHandler(BaseHandler):
@@ -303,6 +290,83 @@ class TestHandler(BaseHandler):
 
         self.write(crl.fp.getvalue())
        
+class ComposeHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        id = self.get_argument("id", None)
+        entry = None
+        if id:
+            entry = self.db.get("SELECT * FROM entries WHERE id = %s", int(id))
+        self.render("compose.html", entry=entry)
+
+    @tornado.web.authenticated
+    def post(self):
+        id = self.get_argument("id", None)
+        title = self.get_argument("title")
+        text = self.get_argument("markdown")
+        html = markdown.markdown(text)
+        if id:
+            entry = self.db.get("SELECT * FROM entries WHERE id = %s", int(id))
+            if not entry: raise tornado.web.HTTPError(404)
+            slug = entry.slug
+            self.db.execute(
+                "UPDATE entries SET title = %s, markdown = %s, html = %s "
+                "WHERE id = %s", title, text, html, int(id))
+        else:
+            slug = unicodedata.normalize("NFKD", title).encode(
+                "ascii", "ignore")
+            slug = re.sub(r"[^\w]+", " ", slug)
+            slug = "-".join(slug.lower().strip().split())
+            if not slug: slug = "entry"
+            while True:
+                e = self.db.get("SELECT * FROM entries WHERE slug = %s", slug)
+                if not e: break
+                slug += "-2"
+            self.db.execute(
+                "INSERT INTO entries (author_id,title,slug,markdown,html,"
+                "published) VALUES (%s,%s,%s,%s,%s,UTC_TIMESTAMP())",
+                self.current_user.id, title, slug, text, html)
+        self.redirect("/entry/" + slug)
+class AuthLoginHandler(BaseHandler, tornado.auth.GoogleMixin):
+    @tornado.web.asynchronous
+    def get(self):
+        if self.get_argument("openid.mode", None):
+            self.get_authenticated_user(self.async_callback(self._on_auth))
+            return
+        self.authenticate_redirect()
+
+    def _on_auth(self, user):
+        print "========================="
+        print user
+        print "========================="
+        if not user:
+            raise tornado.web.HTTPError(500, "Google auth failed")
+
+
+
+        author = self.db.get("SELECT * FROM authors WHERE email = %s",user["email"])
+        print author
+        if not author:
+            # Auto-create first author
+            any_author = self.db.get("SELECT * FROM authors LIMIT 1")
+
+            print "================================ any_author"
+            print any_author
+
+            if not any_author:
+                author_id = self.db.execute(
+                    "INSERT INTO authors (email,name) VALUES (%s,%s)",
+                    user["email"], user["name"])
+            else:
+                self.redirect("/")
+                return
+        else:
+            author_id = author["id"]
+            print "============== author_id "
+            print author_id
+
+        self.set_secure_cookie("user", str(author_id))
+        self.redirect(self.get_argument("next", "/"))
 
 def main():
     tornado.options.parse_command_line()
