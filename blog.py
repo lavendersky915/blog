ff8285ae4f186869b8eea94a6acbbdf163128458
lavender <lavendersky915@gmail.com>
2013-03-26 14:53:30 +0800

    content

--- blog.py
+++ blog.py
@@ -212,7 +212,7 @@ class GoogleHandler(BaseHandler):
 
         data = tornado.escape.json_encode(allass)
         #self.render("google.html", entries="test")
-        self.write(allass)
+        self.write(content)
 
 class Lavender_STPI(BaseHandler):
     def get(self):
