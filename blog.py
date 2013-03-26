37c42ef49e477986d8fd18444b2ee4432a8ed01b
lavender <lavendersky915@gmail.com>
2013-03-26 12:59:02 +0800

    test

--- blog.py
+++ blog.py
@@ -248,15 +248,13 @@ class Lavender_STPI(BaseHandler):
 
                 #找出訴訟名稱裡的原套被告
                 litiname = a.split('訴訟名稱')
-                liticom = litiname[1].split('提告日期')
-                c = liticom[0].count("v.")
                 
 
             pass
         pass
 
         data = tornado.escape.json_encode(c)
-        self.write(data)
+        self.write(litiname[1])
     
 
 class MLStripper(HTMLParser):
