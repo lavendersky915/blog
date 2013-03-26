62bd1c761961b54903c9559c35017ceaffe00caf
lavender <lavendersky915@gmail.com>
2013-03-26 13:25:29 +0800

    length

--- blog.py
+++ blog.py
@@ -260,13 +260,14 @@ class Lavender_STPI(BaseHandler):
 
                 detemp = litiname[1].split('被告')
                 dename = detemp[1].split('案號')
-                te = dename[0].split('<BR>')
+                decom = dename[0].split('<BR>')
+                length = len(decom)
 
             pass
             pass
 
-        data = tornado.escape.json_encode(c)
-        self.write(te[1])
+        data = tornado.escape.json_encode(length)
+        self.write(data)
     
 
 class MLStripper(HTMLParser):
