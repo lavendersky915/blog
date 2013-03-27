2d5bc29d4f8d98ce2650874e4aee33150451f95c
lavender <lavendersky915@gmail.com>
2013-03-27 14:54:50 +0800

    test

--- blog.py
+++ blog.py
@@ -187,6 +187,7 @@ class GoogleHandler(BaseHandler):
         allass = ""
         alllink =""
         assarray = []
+        tw = ""
         p = 'Primary'
         for x in xrange(0,count):
             html = obj_result['items'][x]['link']
@@ -206,9 +207,7 @@ class GoogleHandler(BaseHandler):
             ans = soup.find("div", { "class" : "about_content" })
             content = strip_tags(ans.prettify())
             array = content.split('Assignee')
-            if p in array[1]:
-                tw = "w"
-            pass
+            
         pass
 
         data = tornado.escape.json_encode(obj_result)
