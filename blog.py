b01891c5f49ac945b799d57daa89fb562b3153b0
lavender <lavendersky915@gmail.com>
2013-03-27 13:54:54 +0800

    see link

--- blog.py
+++ blog.py
@@ -179,40 +179,24 @@ class FeedHandler(BaseHandler):
 class GoogleHandler(BaseHandler):
     def get(self):
         keyword = self.get_argument("keyword", default=None, strip=False)
-        url = "https://www.googleapis.com/customsearch/v1?q="+keyword+"&key=AIzaSyCyj6LcvbjCciGMmt9Vq2UXUfShev_IpWM&cx=005971756043172606388:5upt-glxmyc"
+        url = "https://www.googleapis.com/customsearch/v1?q="+keyword+"&start=11&key=AIzaSyCyj6LcvbjCciGMmt9Vq2UXUfShev_IpWM&cx=005971756043172606388:5upt-glxmyc"
         result = urllib.urlopen(url).read()
         count = result.count('kind') - 1
         obj_result = tornado.escape.json_decode(result)
         content = ""
         allass = ""
+        alllink =""
         assarray = []
         for x in xrange(0,count):
             html = obj_result['items'][x]['link']
             link = str(html)
-            crl = pycurl.Curl()
-            crl.setopt(pycurl.VERBOSE,1)
-            crl.setopt(pycurl.FOLLOWLOCATION, 1)
-            crl.setopt(pycurl.MAXREDIRS, 5)
-            crl.fp = StringIO.StringIO()
-            crl.setopt(pycurl.URL, link)
-            crl.setopt(crl.WRITEFUNCTION, crl.fp.write)
-            crl.perform()
-
-            soup = BeautifulSoup(crl.fp.getvalue())
+            alllink = alllink + link +"<br>"
             
-            ans = soup.find("div", { "class" : "about_content" })
-            content = strip_tags(ans.prettify())
-            array = content.split('Assignee')
-            arr = array[1].split('Primary')
-            ass = arr[0].split(':')
-            allass = allass + ass[1] + "<br>"
-            assarray.append(ass[1])
-            leng = len(assarray)
         pass
 
         data = tornado.escape.json_encode(obj_result)
         #self.render("google.html", entries="test")
-        self.write(assarray[0])
+        self.write(alllink)
 
 class Lavender_STPI(BaseHandler):
     def get(self):
