c0ba2fdb84948af8ba22dce1a029c5ca5b75ff44
lavender <lavendersky915@gmail.com>
2013-03-28 17:19:05 +0800

    ss

--- blog.py
+++ blog.py
@@ -183,7 +183,7 @@ class GoogleHandler(BaseHandler):
         page = 1
         linkall =""
         keyword = self.get_argument("keyword", default=None, strip=False)
-        #while page < 50:
+        while page < 50:
             index = str(page)
             url = "https://www.googleapis.com/customsearch/v1?q="+keyword+"&start="+index+"&key=AIzaSyCyj6LcvbjCciGMmt9Vq2UXUfShev_IpWM&cx=005971756043172606388:5upt-glxmyc"
             result = urllib.urlopen(url).read()
@@ -212,8 +212,8 @@ class GoogleHandler(BaseHandler):
                     leng = len(assarray)
                 pass
             pass
-            #page = page + 10
-        #pass
+            page = page + 10
+        pass
 
         data = tornado.escape.json_encode(leng)
         #self.render("google.html", entries="test")
