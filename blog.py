ae45be1deb2ecd1810a2af713d1571ba1dc1bd43
lavender <lavendersky915@gmail.com>
2013-03-31 20:58:39 +0800

    test

--- blog.py
+++ blog.py
@@ -254,20 +254,22 @@ class Lavender_STPI(BaseHandler):
 
                 #找出訴訟名稱裡的原套被告
                 litiname = a.split('訴訟名稱')
-                liticom = litiname[1].split('提告日期')
-                c = liticom[0].count("v.")
-                if c == 1:
-                    twocom = liticom[0].split('v.') 
-                    p.append(twocom[0])
-                    d.append(twocom[1])
-                    two = "原告" + p[0] + "被告" + d[0]
+                if litiname[1] == "":
+                    
+                    liticom = litiname[1].split('提告日期')
+                    c = liticom[0].count("v.")
+                    if c == 1:
+                        twocom = liticom[0].split('v.') 
+                        p.append(twocom[0])
+                        d.append(twocom[1])
+                        two = "原告" + p[0] + "被告" + d[0]
+                    pass
+
+                    detemp = litiname[1].split('被告')
+                    dename = detemp[1].split('案號')
+                    decom = dename[0].split('<BR>')
+                    length = len(decom)
                 pass
-
-                detemp = litiname[1].split('被告')
-                dename = detemp[1].split('案號')
-                decom = dename[0].split('<BR>')
-                length = len(decom)
-
             pass
             pass
 
