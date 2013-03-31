fa912ff76a690c0d0699550e79078bd48c1537a3
lavender <lavendersky915@gmail.com>
2013-04-01 00:52:42 +0800

    7799

--- blog.py
+++ blog.py
@@ -229,7 +229,6 @@ class Lavender_STPI(BaseHandler):
         length =""
         p = []
         d = []
-        aa= ""
         w = unicode('告', 'utf-8')
 
         keyword = self.get_argument("keyword", default=None, strip=False)
@@ -247,8 +246,8 @@ class Lavender_STPI(BaseHandler):
                 if  w in obj_result['items'][x]['title']:
                     litigation = litigation+1
                     test = obj_result['items'][x]['link']
-                    links = str(test)
-                    aa = aa+ links +"<br>"
+                    #links = str(test)
+                    links = "http://iknow.stpi.narl.org.tw/Post/Read.aspx?PostID=7799"
                     if links is not None:
                         crl = pycurl.Curl()
                         crl.setopt(pycurl.VERBOSE,1)
@@ -262,15 +261,26 @@ class Lavender_STPI(BaseHandler):
 
                         #找出訴訟名稱裡的原套被告
                         litiname = a.split('訴訟名稱')
-                       
-                        
+                        liticom = litiname[1].split('提告日期')
+                        c = liticom[0].count("v.")
+                        if c == 1:
+                            twocom = liticom[0].split('v.') 
+                            p.append(twocom[0])
+                            d.append(twocom[1])
+                            two = "原告" + p[0] + "被告" + d[0]
+                        pass
+
+                        detemp = litiname[1].split('被告')
+                        dename = detemp[1].split('案號')
+                        decom = dename[0].split('<BR>')
+                        length = len(decom)
                     pass
                 pass
             pass
             pages = pages + 10
         pass
-        data = tornado.escape.json_encode(a)
-        self.write(aa)
+        data = tornado.escape.json_encode(length)
+        self.write(data)
     
 
 class MLStripper(HTMLParser):
