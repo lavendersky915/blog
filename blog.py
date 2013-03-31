0e28cdac6aa5a637d638366b9db157df5a579e7a
lavender <lavendersky915@gmail.com>
2013-03-31 22:01:18 +0800

    aa

--- blog.py
+++ blog.py
@@ -259,6 +259,9 @@ class Lavender_STPI(BaseHandler):
                         crl.perform()
                         a = crl.fp.getvalue()
 
+                        #找出訴訟名稱裡的原套被告
+                        litiname = a.split('訴訟名稱')
+                        liticom = litiname[1].split('提告日期')
                         
                     pass
                 pass
@@ -266,7 +269,7 @@ class Lavender_STPI(BaseHandler):
             pages = pages + 10
         pass
         data = tornado.escape.json_encode(obj_result)
-        self.write(a)
+        self.write(litiname[1])
     
 
 class MLStripper(HTMLParser):
