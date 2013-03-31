886df07ae798b106d42639e84b64a78ca4c3286a
lavender <lavendersky915@gmail.com>
2013-03-31 21:15:16 +0800

    see array

--- blog.py
+++ blog.py
@@ -217,7 +217,7 @@ class GoogleHandler(BaseHandler):
 
         data = tornado.escape.json_encode(leng)
         #self.render("google.html", entries="test")
-        self.write(data)
+        self.write(assarray[0])
 
 class Lavender_STPI(BaseHandler):
     def get(self):
