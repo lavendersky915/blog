6aeb8124ede708a7fb1215f267ee6a6f1d5cf12f
lavender <lavendersky915@gmail.com>
2013-03-31 16:34:16 +0800

    test

--- blog.py
+++ blog.py
@@ -226,53 +226,10 @@ class Lavender_STPI(BaseHandler):
         result = urllib.urlopen(url).read()
         count = result.count('kind') - 1
         obj_result = tornado.escape.json_decode(result)
-        #宣告區
-        test = ""
-        litigation=0
-        two =""
-        p = []
-        d = []
-        w = unicode('告', 'utf-8')
-        name = unicode('訴訟名稱', 'utf-8')
+        
 
-        #找出訴訟並紀錄
-        for x in xrange(0,count):
-            
-            if  w in obj_result['items'][x]['title']:
-                litigation = litigation+1
-                test = obj_result['items'][x]['link']
-                links = str(test)
-                crl = pycurl.Curl()
-                crl.setopt(pycurl.VERBOSE,1)
-                crl.setopt(pycurl.FOLLOWLOCATION, 1)
-                crl.setopt(pycurl.MAXREDIRS, 5)
-                crl.fp = StringIO.StringIO()
-                crl.setopt(pycurl.URL, links)
-                crl.setopt(crl.WRITEFUNCTION, crl.fp.write)
-                crl.perform()
-                a = crl.fp.getvalue()
-
-                #找出訴訟名稱裡的原套被告
-                litiname = a.split('訴訟名稱')
-                liticom = litiname[1].split('提告日期')
-                c = liticom[0].count("v.")
-                if c == 1:
-                    twocom = liticom[0].split('v.') 
-                    p.append(twocom[0])
-                    d.append(twocom[1])
-                    two = "原告" + p[0] + "被告" + d[0]
-                pass
-
-                detemp = litiname[1].split('被告')
-                dename = detemp[1].split('案號')
-                decom = dename[0].split('<BR>')
-                length = len(decom)
-
-            pass
-            pass
-
-        data = tornado.escape.json_encode(length)
-        self.write(data)
+        data = tornado.escape.json_encode(obj_result)
+        self.write(obj_result)
     
 
 class MLStripper(HTMLParser):
