bc143f776b5dd50f7d84c7b327dbd3ec6acaf400
lavender <lavendersky915@gmail.com>
2013-04-01 01:48:50 +0800

    6669

--- blog.py
+++ blog.py
@@ -248,7 +248,7 @@ class Lavender_STPI(BaseHandler):
                     litigation = litigation+1
                     test = obj_result['items'][x]['link']
                     #links = str(test)
-                    links = "http://iknow.stpi.narl.org.tw/Post/Read.aspx?PostID=5657"
+                    links = "http://iknow.stpi.narl.org.tw/Post/Read.aspx?PostID=6669"
                     if links is not None:
                         crl = pycurl.Curl()
                         crl.setopt(pycurl.VERBOSE,1)
@@ -264,26 +264,29 @@ class Lavender_STPI(BaseHandler):
                         pass
                         
                         #找出訴訟名稱裡的原套被告
-                        # litiname = a.split('訴訟名稱')
-                        # liticom = litiname[1].split('提告日期')
-                        # c = liticom[0].count("v.")
-                        # if c == 1:
-                        #     twocom = liticom[0].split('v.') 
-                        #     p.append(twocom[0])
-                        #     d.append(twocom[1])
-                        #     two = "原告" + p[0] + "被告" + d[0]
-                        #     detemp = litiname[1].split('被告')
-                        #     dename = detemp[1].split('案號')
-                        #     decom = dename[0].split('<BR>')
-                        #     length = len(decom)
-                        # pass                        
+                        if check == "y":
+                            litiname = a.split('訴訟名稱')
+                            liticom = litiname[1].split('提告日期')
+                            c = liticom[0].count("v.")
+                            if c == 1:
+                                twocom = liticom[0].split('v.') 
+                                p.append(twocom[0])
+                                d.append(twocom[1])
+                                two = "原告" + p[0] + "被告" + d[0]
+                                detemp = litiname[1].split('被告')
+                                dename = detemp[1].split('案號')
+                                decom = dename[0].split('<BR>')
+                                length = len(decom)
+                            pass           
+                        pass
+                                     
                     pass
                 pass
             pass
             pages = pages + 10
         pass
-        data = tornado.escape.json_encode(a)
-        self.write(check)
+        data = tornado.escape.json_encode(length)
+        self.write(data)
     
 
 class MLStripper(HTMLParser):
