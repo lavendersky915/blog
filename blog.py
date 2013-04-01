1901a9d958d0f9eff520c60722106269ae5c00f6
lavender <lavendersky915@gmail.com>
2013-04-01 11:58:50 +0800

    dename[0]

--- blog.py
+++ blog.py
@@ -290,7 +290,7 @@ class Lavender_STPI(BaseHandler):
             pages = pages + 10
         pass
         data = tornado.escape.json_encode(length)
-        self.write(detemp[1])
+        self.write(dename[0])
     
 
 class MLStripper(HTMLParser):
