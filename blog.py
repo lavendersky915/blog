1ab7dd980f7c072672be81752c4aa5bad1cf640a
lavender <lavendersky915@gmail.com>
2013-04-04 15:43:32 +0800

    test

--- blog.py
+++ blog.py
@@ -217,7 +217,7 @@ class GoogleHandler(BaseHandler):
             page = page + 10
         pass
         data = tornado.escape.json_encode(leng)
-        self.write(assgtable)
+        self.write(arr[0])
 
 class Lavender_STPI(BaseHandler):
     def get(self):
