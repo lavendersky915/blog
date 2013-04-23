b9fcedb59b15572426156fa1e9d38a250e6ee081
lavender <lavendersky915@gmail.com>
2013-04-23 18:30:11 +0800

    test

--- blog.py
+++ blog.py
@@ -40,7 +40,7 @@ from bs4 import BeautifulSoup
 
 define("port", default=8888, help="run on the given port", type=int)
 define("mysql_host", default="127.0.0.1:3306", help="blog database host")
-define("mysql_database", default="blog", help="blog database name")
+define("mysql_database", default="IPPlusDB", help="blog database name")
 define("mysql_user", default="root", help="blog database user")
 define("mysql_password", default="19052260", help="blog database password")
 
@@ -315,14 +315,11 @@ def strip_tags(html):
 
 class TestHandler(BaseHandler):
     def get(self):
-        keyword = self.get_argument("keyword", default=None, strip=False)
-        p = 0
-        while p<10:
-            p=p+1
-            pass
-        t = str(p)
+        s = self.db.execute(
+            "INSERT INTO Cache (Ca_keyword) VALUES (%s)",
+            "keyword")
 
-        self.write(t)
+    self.write(s)
        
 class ComposeHandler(BaseHandler):
     @tornado.web.authenticated
