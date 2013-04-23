08b3ce5bc3fb54ee9be1e1bd10cb425470d8764b
lavender <lavendersky915@gmail.com>
2013-04-23 18:41:44 +0800

    origin

--- blog.py
+++ blog.py
@@ -40,7 +40,7 @@ from bs4 import BeautifulSoup
 
 define("port", default=8888, help="run on the given port", type=int)
 define("mysql_host", default="127.0.0.1:3306", help="blog database host")
-define("mysql_database", default="IPPlusDB", help="blog database name")
+define("mysql_database", default="blog", help="blog database name")
 define("mysql_user", default="root", help="blog database user")
 define("mysql_password", default="19052260", help="blog database password")
 
@@ -70,7 +70,6 @@ class Application(tornado.web.Application):
             cookie_secret="123456",
             login_url="/auth/login",
             autoescape=None,
-            debug=True
         )
         tornado.web.Application.__init__(self, handlers, **settings)
         # Have one global connection to the blog DB across all handlers
@@ -316,10 +315,14 @@ def strip_tags(html):
 
 class TestHandler(BaseHandler):
     def get(self):
-    self.db.execute(
-        "INSERT INTO Cache (Ca_keyword) VALUES (%s)", "keyword")
+        keyword = self.get_argument("keyword", default=None, strip=False)
+        p = 0
+        while p<10:
+            p=p+1
+            pass
+        t = str(p)
 
-    
+        self.write(t)
        
 class ComposeHandler(BaseHandler):
     @tornado.web.authenticated
